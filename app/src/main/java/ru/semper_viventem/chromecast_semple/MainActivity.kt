68128087e28d7dc3015198dd4e39f8c6e0316788
Constantine <kostyaxxx8@gmail.com>
2019-02-20 22:10:55 +0300

    Create project

---
+++ app/src/main/java/ru/semper_viventem/chromecast_semple/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.semper_viventem.chromecast_semple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
