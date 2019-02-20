572e901438606fa0a1559650126023dcf0c1161f
Constantine <kostyaxxx8@gmail.com>
2019-02-20 23:26:12 +0300

    Create player interface

---
+++ app/src/main/java/ru/semper_viventem/chromecast_semple/player/MediaEntities.kt
@@ -0,0 +1,20 @@
+package ru.semper_viventem.chromecast_semple.player
+
+import android.net.Uri
+
+data class MediaContent(
+    val type: Type,
+    val contentUris: List<Uri>,
+    val metadata: MediaMetadata? = null
+) {
+    enum class Type {
+        VIDEO,
+        AUDIO
+    }
+}
+
+data class MediaMetadata(
+    val author: String = "",
+    val posterUrl: String = "",
+    val title: String = ""
+)
---
+++ app/src/main/java/ru/semper_viventem/chromecast_semple/player/Player.kt
@@ -0,0 +1,45 @@
+package ru.semper_viventem.chromecast_semple.player
+
+interface Player {
+
+    val isPlaying: Boolean
+
+    val isReleased: Boolean
+
+    val duration: Long
+
+    var positionInMillis: Long
+
+    var speed: Float
+
+    var volume: Float
+
+    var loop: Boolean
+
+    fun addListener(listener: PlayerCallback)
+
+    fun removeListener(listener: PlayerCallback): Boolean
+
+    fun prepare(mediaContent: MediaContent)
+
+    fun play()
+
+    fun pause()
+
+    fun release()
+
+    interface PlayerCallback {
+        fun onPlaying(currentPosition: Long)
+        fun onPaused(currentPosition: Long)
+        fun onPreparing()
+        fun onPrepared()
+        fun onLoadingChanged(isLoading: Boolean)
+        fun onDurationChanged(duration: Long)
+        fun onSetSpeed(speed: Float)
+        fun onSeekTo(fromTimeInMillis: Long, toTimeInMillis: Long)
+        fun onWaitingForNetwork()
+        fun onError(error: String?)
+        fun onReleased()
+        fun onPlayerProgress(currentPosition: Long)
+    }
+}
